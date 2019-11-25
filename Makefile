6ba92dfbf9d01a137668f3bdcaba60770cc34ab5
Timothe Rifflet <trifflet@student.le-101.fr>
2019-11-25 14:12:14 +0100

    Fix Multiple GNL calls, prettied tests

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: trifflet <trifflet@student.le-101.fr>      +:+   +:    +:    +:+      #
 #                                                  #+#   #+    #+    #+#       #
 #    Created: 2019/10/07 12:41:36 by trifflet     #+#   ##    ##    #+#        #
-#    Updated: 2019/11/13 15:11:06 by trifflet    ###    #+. /#+    ###.fr      #
+#    Updated: 2019/11/23 17:18:46 by trifflet    ###    #+. /#+    ###.fr      #
 #                                                          /                   #
 #                                                         /                    #
 # **************************************************************************** #
@@ -22,15 +22,15 @@ SRC			=	get_next_line.c			\
 OBJ			=	${SRC:.c=.o}
 
 FLAGS		=	-Wall -Wextra -Werror -g
-DFLAGS		=	-D BUFFER_SIZE=64
+DFLAGS		=	-D BUFFER_SIZE=90
 
 %.o: %.c $(HEADER)
-			gcc $(FLAGS) $(DFLAGS) -c $< -o ${<:.c=.o}
+			gcc -g $(FLAGS) $(DFLAGS) -c $< -o ${<:.c=.o}
 
 all:    	$(NAME)
 
 $(NAME):	$(OBJ)
-			gcc $(FLAGS) $(DFLAGS) -o $(NAME) $(OBJ)
+			gcc -g $(FLAGS) $(DFLAGS) -o $(NAME) $(OBJ)
 
 clean:
 			rm -rf $(OBJ) $(OBJ_BONUS)
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: trifflet <trifflet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/21 16:37:48 by trifflet     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/12 17:02:36 by trifflet    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/25 14:07:47 by trifflet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,6 +14,11 @@
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
+
+#define RED "\033[0;31m"
+#define YLW "\033[0;33m"
+#define BLU "\033[0;34m"
+#define RST "\033[0m"
 
 int	main(int ac, char **av)
 {
@@ -24,14 +29,19 @@ int	main(int ac, char **av)
 	control = 1;
 	text = NULL;
 	if (ac == 1)
-	fd = 0;
+		fd = 0;
 	else
-	fd = open(av[1], O_RDONLY);
+		fd = open(av[1], O_RDONLY);
 	while (control > 0)
 	{
 		control = get_next_line(fd, &text);
-		printf("%d, %s\n", control, text);
+		printf(BLU"%d: "YLW"%p"RST" "RED"|"RST"%s"RED"|"RST"\n",\
+			control, text, text);
 		free(text);
 	}
+	control = get_next_line(fd, &text);
+	printf(BLU"%d: "YLW"%-14p"RST" "RED"|"RST"%s"RED"|"RST"\n",\
+		control, text, text);
+	free(text);
 	return (0);
 }
